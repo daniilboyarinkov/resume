85d8d4ed683f2f3034bf75f5169ad1af03fb2430
dboyarinkov <dboyarinkov@yandex-team.ru>
2024-01-21 22:43:44 +0300

    update resume

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const contactsContainer = document.querySelector('.contacts');
 
 const contactsContent = contacts.map(c => {
   return `
-  <div class="contact">
+  <li class="contact">
     <img class="contact-logo" src="${c.icon}" />
-    <span>${c.contact}</span>
-  </div>
+    <p>${c.contact}</p>
+  </li>
   `;
 }).join('');
 
@@ -15,29 +15,17 @@ const skillsContainer = document.querySelector('.skills');
 
 const skillsContent =  skills.map(s => {
   return `
-  <div class="skill">
-    <img class="skill-logo" src="${s.icon}" />
-    <span>${s.id}</span>
-  </div>
+  <li class="skill">
+    <img class="skill-logo" src="${s.icon}" alt="${s.id}" />
+    <p>${s.id}</p>
+  </li>
   `;
 }).join('');
 
 skillsContainer.innerHTML = skillsContent;
 
-
-const educationContainer = document.querySelector('.education');
-
-educationContainer.innerHTML = `
-  <ul class="block">
-    ${mainContent[0].content.map(s => `<li>${s}</li>`).join('')}
-  </ul>
-`;
-
-
 const personalContainer = document.querySelector('.personal');
 
 personalContainer.innerHTML = `
-  <ul class="block">
-    ${mainContent[1].content.map(s => `<li>${s}</li>`).join('')}
-  </ul
+  ${mainContent[0].content.map(s => `<li><p>${s}</p></li>`).join('')}
 `;
